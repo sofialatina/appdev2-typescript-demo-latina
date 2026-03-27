4a766a899ea036677fb24e8defb902a45a91d1cd
Sofia Latina <sofiaisabellatina@gmail.com>
2026-03-27 16:48:59 +0800

    Done playing and updating the 05-enum.ts

--- 05-enum.ts
+++ 05-enum.ts
@@ -2,13 +2,20 @@
     enum Role {
         // SuperAdmin,
         // SuperAdmin = 1,
-        SuperAdmin = 'SuperAdmin',
+        Painter = 'Painter',
         // Teacher,
-        Teacher = 'Teacher',
+        Lawyer = 'Lawyer',
         // Student,
-        Student = 'Student',
+        Mentor = 'Mentor',
     }
 
     // let userRole: Role = 0;
-    let userRole: Role = Role.SuperAdmin;
-})
+    let userRole1: Role = Role.Painter;
+    console.log("User Role 1:", userRole1);
+
+    let userRole2: Role = Role.Lawyer;
+    console.log("User Role 2:", userRole2);
+
+    let userRole3: Role = Role.Mentor;
+    console.log("User Role 3:", userRole3);
+})();
