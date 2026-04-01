06dd24b4d8a2c1c32e6cf475581f00ba41ca08a8
Sofia Latina <sofiaisabellatina@gmail.com>
2026-04-01 14:04:10 +0800

    Done playing and updating the 09-special-types.ts

--- 09-special-types.ts
+++ 09-special-types.ts
@@ -1,25 +1,26 @@
 // Special Type - null
 let a: null;
-
-a = null
+a = null;
 // a = "Hi"
 
-let b: null | string
-
-b = null
-b = "Hi"
+let b: null | string;
+b = null;
+b = "Allow me to lend you some moonlight.";
 // b = 10
 
 // Special Type - undefined
 let c: undefined;
-
-c = undefined
+c = undefined;
 // c = null
 // c = "Hi"
 
-let d: undefined | string
-
-d = undefined
-d = "Hi"
+let d: undefined | string;
+d = undefined;
+d = "May the wind guide you.";
 // d = null
 // d = 10
+
+console.log("a:", a);
+console.log("b:", b);
+console.log("c:", c);
+console.log("d:", d);
