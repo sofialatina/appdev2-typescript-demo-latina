78f3614499959c8883ea8944b12ae6b5584e52f7
Sofia Latina <sofiaisabellatina@gmail.com>
2026-04-01 13:44:22 +0800

    Done playing and updating the 07-custom-type-role.ts

--- 07-custom-type-role.ts
+++ 07-custom-type-role.ts
@@ -1,7 +1,7 @@
 (() => {
     // Type Aliases or Custom Types
     // use `type` keyword to create custom type
-    type Role = 'admin' | 'guess' | 'teacher' | 'student'
+    type Role = 'admin' | 'guess' | 'teacher' | 'student';
 
     // Complex Object Type Definition
     type User = {
@@ -9,13 +9,31 @@
         age: number;
         role: Role; // you can use as well the Role type in Object type
         permission: string[];
-    }
+    };
 
-    let userRole: Role = 'admin'
+    let userRole: Role = 'student';
+    const user: User = {
+        name: 'Marius',
+        age: 21,
+        role: 'student',
+        permission: ['access to painting room to paint', 'access key to nxx headquarters' ]
+    };
+    console.log("User Role", userRole);
+    console.log("User Info:", user);
 
-    // ...
+    let userRole1: Role = 'teacher';
+    const user1: User = {
+        name: 'Vyn',
+        age: 27,
+        role: 'teacher',
+        permission: ['teaches at stellis university', 'owner of nxx headquarters' ]
+    };
+    console.log("User Role 1", userRole1);
+    console.log("User Info 1:", user1);
 
     function access(role: Role) {
-        // ...
+        console.log("Role:", role);
     }
-})
+    access(user.role);
+    access(user1.role);
+})();
