208b727e8407f396c10ad46dd15487bddf36f946
Sofia Latina <sofiaisabellatina@gmail.com>
2026-04-01 13:54:50 +0800

    Done playing and updating the 08-functions.ts

--- 08-functions.ts
+++ 08-functions.ts
@@ -5,22 +5,22 @@
         return a + b
     }
 
-    add(5, 10)
+    console.log("Add Result:" , add(5, 10));
     // add("5", 10)
 
     // Special Type `void` - return nothing
     // Function doesn't return a meaningful value, but it does finish
     function log(message: string): void {
-        console.log(message)
+        console.log(message);
     }
 
-    log("Hello, World!")
+    log("Hello, World!");
 
     // Another Special Type `never`
     // Function never finishes normally (throws error, infinite loop)
-    function logAndThrow(errorMessage: string) {
-        console.log(errorMessage)
-        throw new Error(errorMessage)
+    function logAndThrow(errorMessage: string): never {
+        console.log("Error:", errorMessage);
+        throw new Error(errorMessage);
     }
 
     // Function as Types
@@ -30,11 +30,11 @@
     // () => void - function type
     function performJob(cb: (m: string) => void) {  // you can also use cb: Function but () => void is the best practice
         // ...
-        cb('Job Done!')
+        cb('Well Done!')
     }
 
     let logMsg = (msg: string): void => {
-        console.log(msg)
+        console.log(msg);
     }
 
     performJob(logMsg)
@@ -47,16 +47,16 @@
     }
 
     let user: User = {
-        name: "Elmer",
-        age: 39,
+        name: "Latina",
+        age: 21,
         // greet: function() {
         //     return this.name
         // }
         greet() {
-            console.log(`Hello, ${this.name}`)
-            return this.name
+            console.log(`Hello, ${this.name}`);
+            return this.name;
         }
     }
 
-    user.greet()
-})
+    user.greet();
+})();
