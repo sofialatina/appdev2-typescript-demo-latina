c81f8fdbead2117dacd6adc773494c1ccc6124a1
Sofia Latina <sofiaisabellatina@gmail.com>
2026-04-01 13:21:11 +0800

    Done playing and updating the 06-alternative-to-enum.ts

--- 06-alternative-to-enum.ts
+++ 06-alternative-to-enum.ts
@@ -1,11 +1,14 @@
 (() => {
     // Literal Types (Alternative to Enum)
-    let userRole: 'admin' | 'guess' | 'teacher' | 'student' = 'admin'
+    let userRole: 'Marius' | 'Artem' | 'Rosa' | 'Vyn' | 'Luke' = 'Marius';
 
-    userRole = 'guess'
+    userRole = 'Artem';
     // anotherUserRole = 'superadmin'
 
-    function access(role: 'admin' | 'guess' | 'teacher' | 'student') {
-        // ...
+    function access(role: 'Marius' | 'Artem' | 'Rosa' | 'Vyn' | 'Luke') {
+        console.log("Access role granted to:", role);
+        console.log("Private pass access granted to:", userRole);
     }
-})
+
+    access('Marius');
+})();
